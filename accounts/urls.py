a626b49117123d7b4f9ccc9e1419e0ed9b7092ed
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-08-22 21:43:26 +0900

    password change form completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('edit/', views.profile_edit, name='profile_edit'),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("signup/", views.signup, name="signup"),
+    path("edit/", views.profile_edit, name="profile_edit"),
+    path('password_change/', views.password_change, name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.shortcuts import redirect, render
-from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    LoginView,
+    logout_then_login,
+    PasswordChangeView as AuthPasswordChangeView,
+)
 from django.contrib.auth.decorators import login_required
-from .forms import SignupForm, ProfileForm
+from django.urls import reverse_lazy
+from .forms import SignupForm, ProfileForm, PasswordChangeForm
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
 
@@ -35,14 +41,31 @@ def signup(request):
 
 @login_required
 def profile_edit(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, '프로필 수정 완료')
-            return redirect('profile_edit')
+            messages.success(request, "프로필 수정 완료")
+            return redirect("profile_edit")
     else:
         form = ProfileForm(instance=request.user)
-    return render(request, "accounts/profile_edit.html", {
-        'form': form,
-    })
+    return render(
+        request,
+        "accounts/profile_edit.html",
+        {
+            "form": form,
+        },
+    )
+
+
+class PasswordChangeView(LoginRequiredMixin, AuthPasswordChangeView):
+    success_url = reverse_lazy("password_change")
+    template_name = "accounts/password_change_form.html"
+    form_class = PasswordChangeForm
+
+    def form_valid(self, form):
+        messages.success(self.request, "암호를 변경했습니다.")
+        return super().form_valid(form)
+
+
+password_change = PasswordChangeView.as_view()
