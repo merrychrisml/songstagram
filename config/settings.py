da3ad7c62052ddb2359186814808f0a0ee20f8b9
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-08-22 18:42:34 +0900

    login, logout, signup page make pretty and profile image implemented

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third Apps
     "debug_toolbar",
+    "bootstrap4",
+    "django_pydenticon",
     # Local Apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +64,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'config', "templates"),
+            os.path.join(BASE_DIR, "config", "templates"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -88,6 +91,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -126,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config', "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "config", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
@@ -138,4 +142,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = ["127.0.0.1"]
-
